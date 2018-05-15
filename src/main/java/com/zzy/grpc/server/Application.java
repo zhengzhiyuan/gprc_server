b70dceee841ac8758da4e16adfc71317d11e54a7
zhengzhiyuan <zhengzhiyuan@gomo.com>
2018-05-15 19:41:02 +0800

    去掉连接池配置

--- src/main/java/com/zzy/grpc/server/Application.java
+++ src/main/java/com/zzy/grpc/server/Application.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
@@ -14,7 +15,7 @@ import com.zzy.grpc.server.config.ServiceProperties;
  * 服务启动程序。
  */
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @EnableConfigurationProperties(ServiceProperties.class)
 public class Application {
 
